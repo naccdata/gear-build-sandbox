d1817033e92d8c73aff976bdb05a074f725aea9b
Ben Keller <bjkeller@vt.edu>
2024-10-25 17:32:32 +0000

    Add a gear

---
+++ gear/example_gear/src/python/example_app/run.py
@@ -0,0 +1,14 @@
+"""Entry script for Example Gear."""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+        
+def main():
+    """Main method for Example Gear."""
+
+    log.info("Hello Flywheel!!!")
+
+if __name__ == "__main__":
+    main()
